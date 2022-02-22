c24171db72f7f933e79aeaf04d4a197f3b38dbd5
H3AR7B3A7 <steven.d.hondt.sdh@gmail.com>
2022-02-22 01:30:44 +0100

    :sparkles: AutoConfiguration module and class

---
+++ logging-parent/src/main/java/com/example/loggingparent/MyAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.loggingparent;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:application.properties")
+public class MyAutoConfiguration {
+
+    
+}
